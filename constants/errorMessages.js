410c869700231bfa7a55f3b742a9248c5e021ca5
Eugeniy Burmistrov <eugeniy.burmistrov@devexpress.com>
2018-12-17 17:34:55 +0300

    Fix an error message text

--- constants/errorMessages.js
+++ constants/errorMessages.js
@@ -1,6 +1,6 @@
 module.exports = {
   INVALID_SUBDOMAIN_NAME: {
-    LENGTH: '- Application\'s name must shorter than 63 characters.',
+    LENGTH: '- Application\'s name must be shorter than 63 characters.',
     ALPHA_NUMERIC:
       '- Application name must begin and end with alpha-numeric characters (i.e. letters [A-Za-z] or digits [0-9]).',
     HYPHEN_START_END: '- Application name cannot begin or end with a hyphen.'
