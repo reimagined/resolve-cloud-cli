460cc2249a6049ff4bb1e177465a661f99b036f7
max <mozg.mayhem@gmail.com>
2019-05-04 15:41:40 +0300

    tests: saga properties list

--- __tests__/unit/commands/sagas/properties/list.unit.test.js
+++ __tests__/unit/commands/sagas/properties/list.unit.test.js
@@ -1,1 +1,63 @@
-test.skip('implement', () => {})
+const yargs = require('yargs')
+const {
+  command,
+  aliases,
+  handler,
+  builder,
+  describe: commandDescription
+} = require('../../../../../commands/sagas/properties/list')
+const { get } = require('../../../../../api/client')
+const refreshToken = require('../../../../../refreshToken')
+
+jest.mock('../../../../../api/client', () => ({
+  get: jest.fn()
+}))
+
+jest.mock('../../../../../refreshToken', () => jest.fn(h => (...args) => h('token', ...args)))
+
+const { positional } = yargs
+
+test('command', () => {
+  expect(command).toEqual('list <deployment> <saga>')
+  expect(commandDescription).toEqual(expect.any(String))
+  expect(aliases).toEqual(['ls', '$0'])
+})
+
+test('options', () => {
+  builder(yargs)
+
+  expect(positional).toHaveBeenCalledWith('deployment', {
+    describe: expect.any(String),
+    type: 'string'
+  })
+  expect(positional).toHaveBeenCalledWith('saga', {
+    describe: expect.any(String),
+    type: 'string'
+  })
+  expect(positional).toHaveBeenCalledTimes(2)
+})
+
+describe('handler', () => {
+  afterEach(() => {
+    refreshToken.mockClear()
+    get.mockClear()
+  })
+
+  test('wrapped with refreshToken', async () => {
+    await handler({})
+
+    expect(refreshToken).toHaveBeenCalledWith(expect.any(Function))
+  })
+
+  test('api call', async () => {
+    await handler({
+      deployment: 'deployment-id',
+      saga: 'saga-name'
+    })
+
+    expect(get).toHaveBeenCalledWith(
+      'token',
+      'deployments/deployment-id/sagas/saga-name/properties'
+    )
+  })
+})
