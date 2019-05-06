3221e133f2a1d6f0e79df9216e3edb6381d75e58
max <mozg.mayhem@gmail.com>
2019-05-06 12:52:20 +0300

    tests: list

--- __tests__/unit/commands/list.unit.test.js
+++ __tests__/unit/commands/list.unit.test.js
@@ -1,1 +1,42 @@
-test.skip('implement', () => {})
+const {
+  command,
+  aliases,
+  handler,
+  describe: commandDescription
+} = require('../../../commands/list')
+const { get } = require('../../../api/client')
+const refreshToken = require('../../../refreshToken')
+
+jest.mock('../../../api/client', () => ({
+  get: jest.fn()
+}))
+
+jest.mock('../../../refreshToken', () => jest.fn(h => (...args) => h('token', ...args)))
+
+test('command', () => {
+  expect(command).toEqual('list')
+  expect(commandDescription).toEqual(expect.any(String))
+  expect(aliases).toEqual(['ls'])
+})
+
+describe('handler', () => {
+  afterEach(() => {
+    refreshToken.mockClear()
+    get.mockClear()
+  })
+
+  test('wrapped with refreshToken', async () => {
+    await handler({})
+
+    expect(refreshToken).toHaveBeenCalledWith(expect.any(Function))
+  })
+
+  test('api call', async () => {
+    await handler({
+      deployment: 'deployment-id',
+      saga: 'saga-name'
+    })
+
+    expect(get).toHaveBeenCalledWith('token', 'deployments')
+  })
+})
