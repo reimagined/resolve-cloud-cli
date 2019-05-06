6263f7468b3f54343e062b1ff806b51e725f9bb8
max <mozg.mayhem@gmail.com>
2019-05-06 13:07:54 +0300

    tests: logs

--- __tests__/unit/commands/logs.unit.test.js
+++ __tests__/unit/commands/logs.unit.test.js
@@ -1,1 +1,18 @@
-test.skip('implement', () => {})
+const yargs = require('yargs')
+const { command, builder, describe: commandDescription } = require('../../../commands/logs')
+
+jest.mock('../../../refreshToken', () => jest.fn(h => (...args) => h('token', ...args)))
+
+const { commandDir } = yargs
+
+test('command', () => {
+  expect(command).toEqual('logs')
+  expect(commandDescription).toEqual(expect.any(String))
+})
+
+test('builder', () => {
+  builder(yargs)
+
+  expect(commandDir).toHaveBeenCalledWith('logs')
+  expect(commandDir).toHaveBeenCalledTimes(1)
+})
