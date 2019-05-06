c62bd6bfe795a284176f19ba3fdb9767b1d06ed9
max <mozg.mayhem@gmail.com>
2019-05-06 13:09:30 +0300

    tests: read-models

--- __tests__/unit/commands/read-models.unit.test.js
+++ __tests__/unit/commands/read-models.unit.test.js
@@ -1,1 +1,18 @@
-test.skip('implement', () => {})
+const yargs = require('yargs')
+const { command, builder, describe: commandDescription } = require('../../../commands/read-models')
+
+jest.mock('../../../refreshToken', () => jest.fn(h => (...args) => h('token', ...args)))
+
+const { commandDir } = yargs
+
+test('command', () => {
+  expect(command).toEqual('read-models')
+  expect(commandDescription).toEqual(expect.any(String))
+})
+
+test('builder', () => {
+  builder(yargs)
+
+  expect(commandDir).toHaveBeenCalledWith('read-models')
+  expect(commandDir).toHaveBeenCalledTimes(1)
+})
