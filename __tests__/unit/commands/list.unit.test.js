ffdf9acf6e605f6ac000fa796e674425a8614425
max <mozg.mayhem@gmail.com>
2019-05-06 19:25:55 +0300

    list: implemented on server & printed

--- __tests__/unit/commands/list.unit.test.js
+++ __tests__/unit/commands/list.unit.test.js
@@ -1,3 +1,5 @@
+const columnify = require('columnify')
+const { out } = require('../../../utils/std')
 const {
   command,
   aliases,
@@ -10,8 +12,20 @@ const refreshToken = require('../../../refreshToken')
 jest.mock('../../../api/client', () => ({
   get: jest.fn()
 }))
-
 jest.mock('../../../refreshToken', () => jest.fn(h => (...args) => h('token', ...args)))
+jest.mock('../../../utils/std', () => ({
+  out: jest.fn()
+}))
+
+beforeAll(() => {
+  get.mockResolvedValue({
+    result: [
+      {
+        data: 'data'
+      }
+    ]
+  })
+})
 
 test('command', () => {
   expect(command).toEqual('list')
@@ -23,12 +37,23 @@ describe('handler', () => {
   afterEach(() => {
     refreshToken.mockClear()
     get.mockClear()
+    out.mockClear()
+    columnify.mockClear()
   })
 
   test('wrapped with refreshToken', async () => {
     await handler({})
 
     expect(refreshToken).toHaveBeenCalledWith(expect.any(Function))
+  })
+
+  test('formatted output', async () => {
+    columnify.mockReturnValue('result-output')
+
+    await handler({})
+
+    expect(columnify).toHaveBeenCalledWith([{ data: 'data' }], { minWidth: 30 })
+    expect(out).toHaveBeenCalledWith('result-output')
   })
 
   test('api call', async () => {
--- commands/list.js
+++ commands/list.js
@@ -1,8 +1,20 @@
+const columnify = require('columnify')
 const chalk = require('chalk')
 const refreshToken = require('../refreshToken')
 const { get } = require('../api/client')
+const { out } = require('../utils/std')
 
-const handler = refreshToken(async token => get(token, `deployments`))
+const handler = refreshToken(async token => {
+  const { result } = await get(token, `deployments`)
+
+  if (result) {
+    out(
+      columnify(result, {
+        minWidth: 30
+      })
+    )
+  }
+})
 
 module.exports = {
   handler,
