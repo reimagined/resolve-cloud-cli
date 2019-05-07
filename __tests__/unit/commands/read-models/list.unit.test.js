e5310a2ee4cd6c8ec608d00f27997d06b0b16c80
max <mozg.mayhem@gmail.com>
2019-05-07 23:33:17 +0300

    read-models list: formatted output

--- __tests__/unit/commands/read-models/list.unit.test.js
+++ __tests__/unit/commands/read-models/list.unit.test.js
@@ -1,4 +1,7 @@
+const dateFormat = require('dateformat')
+const columnify = require('columnify')
 const yargs = require('yargs')
+const { out } = require('../../../../utils/std')
 const {
   command,
   aliases,
@@ -12,10 +15,25 @@ const refreshToken = require('../../../../refreshToken')
 jest.mock('../../../../api/client', () => ({
   get: jest.fn()
 }))
-
 jest.mock('../../../../refreshToken', () => jest.fn(h => (...args) => h('token', ...args)))
+jest.mock('../../../../utils/std', () => ({
+  out: jest.fn()
+}))
 
 const { positional } = yargs
+
+beforeAll(() => {
+  get.mockResolvedValue({
+    result: [
+      {
+        name: 'read-model-name',
+        state: 'state',
+        lastEvent: { type: 'event-type', timestamp: 100 },
+        lastError: { message: 'error-message' }
+      }
+    ]
+  })
+})
 
 test('command', () => {
   expect(command).toEqual('list <deployment>')
@@ -37,12 +55,34 @@ describe('handler', () => {
   afterEach(() => {
     refreshToken.mockClear()
     get.mockClear()
+    dateFormat.mockClear()
   })
 
   test('wrapped with refreshToken', async () => {
     await handler({})
 
     expect(refreshToken).toHaveBeenCalledWith(expect.any(Function))
+  })
+
+  test('formatted output', async () => {
+    columnify.mockReturnValue('result-output')
+    dateFormat.mockReturnValue('formatted-date')
+
+    await handler({})
+
+    expect(columnify).toHaveBeenCalledWith(
+      [
+        {
+          name: 'read-model-name',
+          state: 'state',
+          'last event': 'formatted-date event-type',
+          'last error': 'error-message'
+        }
+      ],
+      { minWidth: 30, columns: ['name', 'state', 'last event', 'last error'] }
+    )
+    expect(dateFormat).toHaveBeenCalledWith(new Date(100), expect.any(String))
+    expect(out).toHaveBeenCalledWith('result-output')
   })
 
   test('api call', async () => {
--- commands/read-models/list.js
+++ commands/read-models/list.js
@@ -1,10 +1,31 @@
+const columnify = require('columnify')
+const dateFormat = require('dateformat')
 const chalk = require('chalk')
 const refreshToken = require('../../refreshToken')
 const { get } = require('../../api/client')
+const { out } = require('../../utils/std')
 
-const handler = refreshToken(async (token, { deployment }) =>
-  get(token, `deployments/${deployment}/read-models`)
-)
+const handler = refreshToken(async (token, { deployment }) => {
+  const { result } = await get(token, `deployments/${deployment}/read-models`)
+  if (result) {
+    out(
+      columnify(
+        result.map(({ name, state, lastEvent, lastError }) => ({
+          name,
+          state,
+          'last event': lastEvent
+            ? `${dateFormat(new Date(lastEvent.timestamp), 'm/d/yy HH:MM:ss')} ${lastEvent.type}`
+            : 'N\\A',
+          'last error': lastError ? `${lastError.message}` : 'N\\A'
+        })),
+        {
+          minWidth: 30,
+          columns: ['name', 'state', 'last event', 'last error']
+        }
+      )
+    )
+  }
+})
 
 module.exports = {
   handler,
