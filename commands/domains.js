32a481361eda86e8b534c0c32d52c68c10d95f2e
max <mozg.mayhem@gmail.com>
2019-08-28 19:10:12 +0300

    Domain verification

--- commands/domains.js
+++ commands/domains.js
@@ -2,6 +2,7 @@ const chalk = require('chalk')
 
 module.exports = {
   command: 'domains',
+  aliases: ['domain'],
   describe: chalk.green('manage custom domains'),
   builder: yargs => yargs.commandDir('domains')
 }
