3adf0e84ae0c01bbe4b5b88395451ec7329b7ab4
max <mozg.mayhem@gmail.com>
2019-09-20 14:24:18 +0300

    New 'verbosity' option

--- __tests__/unit/middleware.unit.test.js
+++ __tests__/unit/middleware.unit.test.js
@@ -7,8 +7,14 @@ beforeAll(() => {
   latestVersion.mockReturnValue(packageVersion)
 })
 
+let defaultOptions
+
 beforeEach(() => {
   consola.level = 123
+  defaultOptions = {
+    verbose: false,
+    verbosity: ''
+  }
 })
 
 afterEach(() => {
@@ -17,25 +23,74 @@ afterEach(() => {
 })
 
 test('default log level', async () => {
-  await middleware({ verbose: false })
+  await middleware(defaultOptions)
 
-  expect(consola.level).toEqual(1)
+  expect(consola.level).toEqual(3)
 })
 
-test('verbose log level', async () => {
-  await middleware({ verbose: true })
+test('verbosity=silent log level', async () => {
+  await middleware({
+    ...defaultOptions,
+    verbosity: 'silent'
+  })
+
+  expect(consola.level).toEqual(-1)
+})
+
+test('verbosity=normal log level', async () => {
+  await middleware({
+    ...defaultOptions,
+    verbosity: 'normal'
+  })
+
+  expect(consola.level).toEqual(3)
+})
+
+test('verbosity=debug log level', async () => {
+  await middleware({
+    ...defaultOptions,
+    verbosity: 'debug'
+  })
+
+  expect(consola.level).toEqual(4)
+})
+
+test('verbosity=trace log level', async () => {
+  await middleware({
+    ...defaultOptions,
+    verbosity: 'trace'
+  })
 
   expect(consola.level).toEqual(5)
 })
 
+test('verbose option equals verbosity=debug', async () => {
+  await middleware({
+    ...defaultOptions,
+    verbose: true
+  })
+
+  expect(consola.level).toEqual(4)
+})
+
+test('prefer verbosity level option', async () => {
+  await middleware({
+    ...defaultOptions,
+    verbose: true,
+    verbosity: 'silent'
+  })
+
+  expect(consola.level).toEqual(-1)
+})
+
 test('latest version of the package requested', async () => {
-  await middleware({ verbose: false })
+  await middleware(defaultOptions)
 
   expect(latestVersion).toHaveBeenCalledWith(packageName)
 })
 
 test('do not inform a user about same package version', async () => {
-  await middleware({ verbose: false })
+  await middleware(defaultOptions)
 
   expect(consola.warn).not.toHaveBeenCalledWith(expect.stringContaining(packageVersion))
 })
@@ -43,7 +98,7 @@ test('do not inform a user about same package version', async () => {
 test('inform a user about new package version', async () => {
   latestVersion.mockReturnValueOnce('999.999.999')
 
-  await middleware({ verbose: false })
+  await middleware(defaultOptions)
 
   expect(consola.warn).toHaveBeenCalledWith(expect.stringContaining('999.999.999'))
 })
@@ -51,7 +106,7 @@ test('inform a user about new package version', async () => {
 test('do not inform a user about older package versions', async () => {
   latestVersion.mockReturnValueOnce('0.0.0')
 
-  await middleware({ verbose: false })
+  await middleware(defaultOptions)
 
   expect(consola.warn).not.toHaveBeenCalledWith(expect.stringContaining('0.0.0'))
 })
--- middleware.js
+++ middleware.js
@@ -4,6 +4,13 @@ const chalk = require('chalk')
 const { gt } = require('semver')
 const { version, name } = require('./package')
 
+const verbosityLevels = {
+  silent: -1,
+  normal: 3,
+  debug: 4,
+  trace: 5
+}
+
 const checkForUpdates = async () => {
   const availableVersion = await latestVersion(name)
   if (gt(availableVersion, version)) {
@@ -17,8 +24,9 @@ const checkForUpdates = async () => {
   }
 }
 
-const middleware = async ({ verbose }) => {
-  log.level = verbose ? 5 : 1
+const middleware = async ({ verbose, verbosity }) => {
+  const actualVerbosity = verbosity || (verbose ? 'debug' : 'normal')
+  log.level = verbosityLevels[actualVerbosity] || 3
   await checkForUpdates()
 }
 
