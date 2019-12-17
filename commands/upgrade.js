9931af307a4f10956352d71a41afd473b467bd34
timbset <timbset@gmail.com>
2019-12-17 15:51:06 +0300

    Simplify command

--- commands/upgrade.js
+++ commands/upgrade.js
@@ -1,47 +1,13 @@
 const log = require('consola')
 const chalk = require('chalk')
-const { post, get } = require('../api/client')
+const { post } = require('../api/client')
 const refreshToken = require('../refreshToken')
-const config = require('../config')
 
-const handler = refreshToken(async (token, { version }) => {
-  const name = config.getPackageValue('name', '')
+const handler = refreshToken(async (token, { deployment, upgradeVersion }) => {
+  log.start(`start application upgrading`)
 
-  if (!name || typeof name !== 'string' || name.trim() === '') {
-    throw Error('incorrect application name (wrong working directory?)')
-  }
-
-  log.start(`upgrading application`)
-  log.trace(`requesting the list of existing deployments`)
-
-  const { result: deployments } = await get(token, 'deployments')
-
-  log.trace(`deployment list received: ${deployments.length} items`)
-  let id
-
-  const nameDeployments = deployments.reduce(
-    (acc, item) => (item.name === name ? acc.concat(item) : acc),
-    []
-  )
-
-  if (nameDeployments.length > 1) {
-    throw Error(`multiple deployments with the same name "${name}" found`)
-  }
-
-  if (nameDeployments.length > 0) {
-    ;[{ id }] = nameDeployments
-  }
-
-  if (!id) {
-    throw Error(`deployment is not found`)
-  }
-
-  log.trace(`deployment id obtained: ${id}`)
-
-  await post(token, 'upgrade', {
-    name,
-    id,
-    version
+  await post(token, `deployments/${deployment}/upgrade`, {
+    version: upgradeVersion
   })
 
   log.success(`application is upgraded`)
@@ -49,12 +15,6 @@ const handler = refreshToken(async (token, { version }) => {
 
 module.exports = {
   handler,
-  command: 'upgrade',
-  describe: chalk.green('upgrades deployed application'),
-  builder: yargs =>
-    yargs.option('version', {
-      describe: 'version to upgrade',
-      type: 'string',
-      default: null
-    })
+  command: 'upgrade <deployment> <upgradeVersion>',
+  describe: chalk.green('upgrades deployed application')
 }
