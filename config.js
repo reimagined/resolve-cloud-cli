fa8bf4476a2ac5d6783fa0d57f7dbb669ec77e4d
Georgy K <36447502+Lykoi18@users.noreply.github.com>
2020-09-10 14:28:50 +0300

    Add compatibility versions validation (#102)

--- config.js
+++ config.js
@@ -2,6 +2,7 @@ const os = require('os')
 const fs = require('fs')
 const rc = require('rc')
 const path = require('path')
+const semver = require('semver')
 const lodashGet = require('lodash.get')
 const lodashSet = require('lodash.set')
 const lodashUnset = require('lodash.unset')
@@ -69,9 +70,38 @@ const getPackageValue = (selector, defaultValue = undefined) => {
   return lodashGet(pkg, selector, defaultValue)
 }
 
+const getResolvePackageVersion = () => {
+  let pkg = null
+  try {
+    pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+  } catch (error) {
+    throw new Error('Failed to read "package.json" file.')
+  }
+
+  const keys = Object.keys(pkg.dependencies)
+  const resolvePackageNames = keys.filter(
+    name => name.startsWith('resolve') && name !== 'resolve-cloud'
+  )
+  const version = pkg.dependencies[resolvePackageNames[0]]
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const name of resolvePackageNames) {
+    if (
+      semver.neq(
+        semver.valid(semver.coerce(pkg.dependencies[name])),
+        semver.valid(semver.coerce(version))
+      )
+    ) {
+      throw new Error('The resolve package versions must be the same.')
+    }
+  }
+  return version
+}
+
 module.exports = {
   get,
   set,
   del,
-  getPackageValue
+  getPackageValue,
+  getResolvePackageVersion
 }
