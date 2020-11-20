4abb9b1053f249ce06f1146eea231eae801d3372
Timofey Traynenkov <timbset@gmail.com>
2020-11-20 12:05:09 +0300

    Hotfix/old versions deploy (#116)
    * Fix impossibility to redeploy old apps
    
    * Add tests

--- constants.js
+++ constants.js
@@ -1,10 +1,11 @@
 module.exports = {
   DEPLOYMENT_STATE_AWAIT_INTERVAL_MS: 3000,
   LATEST_RUNTIME_SPECIFIER: 'latest',
-  COMPATIBILITY_VERSIONS: {
+  MIN_RESOLVE_MINOR: 21,
+  VERSION_COMPATIBILITY_MAP: {
     '3': [25],
     '2': [24],
-    '1': [23, 22],
+    '1': [22, 23],
     '0': [21]
   }
 }
